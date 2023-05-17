0a9da5a7ee9b2eab5982b4c27bb48d8fced3d496
trandangkhoa5495 <trandangkhoa5495@gmail.com>
2023-05-17 17:19:52 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Control from "./component/start-control";
+import Liststuden from "./component/start-list-studen";
+import Form from "./component/start-form-sv";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col-lg-7 grid-margin stretch-card">
+          <div className="card">
+            {/* START CONTROL */}
+            <Control />
+            {/* END CONTROL */}
+            {/* START LIST STUDENT */}
+            <Liststuden />
+            {/* END LIST STUDENT */}
+          </div>
+        </div>
+        {/* START FORM SINH VIEN */}
+        <Form />
+        {/* END FORM SINH VIÊN */}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/start-list-studen.js
@@ -0,0 +1,116 @@
+import React from "react";
+
+class Liststuden extends React.Component {
+  render() {
+    return (
+      <div className="card-body">
+        <h3 className="card-title">Danh sách sinh viên</h3>
+        <div className="table-responsive pt-3">
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Mã sinh viên</th>
+                <th>Tên sinh viên</th>
+                <th>Tuổi</th>
+                <th>Giới tính</th>
+                <th>Hành động</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>SV001</td>
+                <td>Nguyễn Văn A</td>
+                <td>20</td>
+                <td>Nam</td>
+                <td>
+                  <div className="template-demo">
+                    <button
+                      type="button"
+                      className="btn btn-danger btn-icon-text"
+                    >
+                      Xem
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-warning btn-icon-text"
+                    >
+                      Sửa
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-success btn-icon-text"
+                    >
+                      Xóa
+                    </button>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>SV002</td>
+                <td>Nguyễn Văn B</td>
+                <td>21</td>
+                <td>Nữ</td>
+                <td>
+                  <div className="template-demo">
+                    <button
+                      type="button"
+                      className="btn btn-danger btn-icon-text"
+                    >
+                      Xem
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-warning btn-icon-text"
+                    >
+                      Sửa
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-success btn-icon-text"
+                    >
+                      Xóa
+                    </button>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>SV003</td>
+                <td>Nguyễn Văn C</td>
+                <td>19</td>
+                <td>Nam</td>
+                <td>
+                  <div className="template-demo">
+                    <button
+                      type="button"
+                      className="btn btn-danger btn-icon-text"
+                    >
+                      Xem
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-warning btn-icon-text"
+                    >
+                      Sửa
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-success btn-icon-text"
+                    >
+                      Xóa
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Liststuden;

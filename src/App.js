4300ec6acd604b156d4cf4cd5610f63958a8d181
trandangkhoa5495 <trandangkhoa5495@gmail.com>
2023-05-17 17:28:12 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+    };
+  }
+  render() {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <button onClick={() => this.setState({ value: this.state.value + 1 })}>
+          click me
+        </button>
+        <br />
+        <label>{this.state.value}</label>
+      </div>
+    );
+  }
 }
-
 export default App;
